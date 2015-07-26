28f35772c20203e5630286a33e6781452c685db5
jordiabante <jordi.abante@tamu.edu>
2015-07-25 19:41:21 -0500

    Minor improvements

--- src/get_fragment_length_distribution/R/get_fragment_length_distribution.R
+++ src/get_fragment_length_distribution/R/get_fragment_length_distribution.R
@@ -1,6 +1,6 @@
 args <- commandArgs(trailingOnly = TRUE)
 read.table(args[1],sep="\t")->frag
 library(ggplot2)
-ggplot(frag) + geom_bar(alpha = 0.35, position = 'identity',aes(x=V1,y=V2),stat='identity')->plot
+plot<-ggplot(frag)+geom_bar(alpha=0.35,position='identity',aes(x=V1,y=V2),stat='identity')
 ggsave(filename=args[2],plot)
 dev.off()
