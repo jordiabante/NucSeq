37f334abc1417808a283ffb8fec84fc10943050f
jordiabante <jordi.abante@tamu.edu>
2015-07-25 19:36:18 -0500

    Testing stuff

--- src/gaussian_kernel/gaussian_kernel.cpp
+++ src/gaussian_kernel/gaussian_kernel.cpp
@@ -1,9 +1,36 @@
+#include <stdio.h>
 #include <iostream>
- 
+
 using namespace std;
- 
-int main()
-{
-  cout<<"HEY, you, I'm alive! Oh, and Hello World!\n";
-  cin.get();
+
+class fragment {
+    int position;
+    int score;
+    public:
+        fragment (int,int);
+        int area () 
+        {
+            return (position*score);
+        }
+};
+
+fragment::fragment (int b, int c) {
+    position = b;
+    score = c;
+}
+
+int main () {
+    string line;
+
+    fragment x (3,4);
+    cout << "area: " << x.area() << endl;
+    ifstream myfile ("example.txt");
+    if (myfile.is_open())
+    {
+        myfile << "This is a line.\n";
+        myfile << "This is another line.\n";
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
