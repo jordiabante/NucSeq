73f52183c213fa0ccd96722108d0689ad3cf8e5b
jordiabante <jordi.abante@tamu.edu>
2015-08-08 10:15:26 -0500

    Minor performance improvements

--- src/analysis/peak_calling/perl/peak_calling.pl
+++ src/analysis/peak_calling/perl/peak_calling.pl
@@ -13,6 +13,8 @@ my $score_1 = 0;
 my $pos_1 = 0;
 my $score_2 = 0; 
 my $pos_2 = 0;
+my $sum =0;
+my $coverage =0;
 
 # Open chr_file
 my $smooth_fh = gzopen($smooth_gz, "rb") or die("can't open file:$!");
@@ -26,8 +28,11 @@ while ($smooth_fh->gzreadline($_) > 0) {
     my $chr=$line[0];
     my $new_pos=$line[1] ;
     my $new_score=$line[2];
+    # Update coverage
+    $sum += $new_score;
+    $coverage = $sum / $new_pos;
     # Look for local maximum
-    if (($score_2 < $score_1) && ($score_1 > $new_score)){
+    if (($score_2 < $score_1) && ($score_1 > $new_score) && ($new_score > $coverage )){
         print "$chr\t$pos_1\t$score_1\n";
     }
     # Update scores
